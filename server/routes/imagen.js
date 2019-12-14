df1551403d5f86c39af43a53f0f016986db75b18
Alejandro Herrera Merlo <55700700+peckas13@users.noreply.github.com>
2019-12-13 23:49:47 -0600

    Update imagen.js

--- server/routes/imagen.js
+++ server/routes/imagen.js
@@ -8,7 +8,7 @@ const app = express();
 app.get('/imagen/:ruta/:img', (req, res) => {
     let ruta = req.params.ruta;
     let img = req.params.img;
-    let rutaImage = path.resolve(__dirname, `./uploads/${ruta}/${img}`);
+    let rutaImage = path.resolve(__dirname, `../../uploads/${ruta}/${img}`);
     let noImage = path.resolve(__dirname, `../assets/no-image.jpeg`);
     if (fs.existsSync(rutaImage)) {
         return res.sendFile(rutaImage);

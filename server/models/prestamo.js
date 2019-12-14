633c5a3f5297c34655bd73a61c1010007b79f383
AlexHM <alex13pks@gmail.com>
2019-12-13 23:41:01 -0600

    agregadas

--- server/models/prestamo.js
+++ server/models/prestamo.js
@@ -1,38 +1,38 @@
-const mongoose = require('mongoose');
-const uniqueValidator = require('mongoose-unique-validator');
-const Libro = require('./libro');
-const Usuario = require('./usuario');
+    const mongoose = require('mongoose');
+    const uniqueValidator = require('mongoose-unique-validator');
+    const Libro = require('./libro');
+    const Usuario = require('./usuario');
 
-let Schema = mongoose.Schema;
+    let Schema = mongoose.Schema;
 
-let prestamoSchema = new Schema({
+    let prestamoSchema = new Schema({
 
-    codigoLibro: {
-        type: Schema.Types.ObjectId,
-        ref: 'Libro',
-        required: [true, 'Ingresar codigo de libro']
-    },
-    codigoUsuario: {
-        type: Schema.Types.ObjectId,
-        ref: 'Usuario',
-        required: [true, 'Por favor ingresa el codigo de usuario']
-    },
-    fechaSalida: {
-        type: Date,
-        default: Date.now
-    },
-    fechaDevolucion: {
-        type: Date
-    },
-    estado: {
-        type: Boolean,
-        default: true
-    }
-});
+        codigoLibro: {
+            type: Schema.Types.ObjectId,
+            ref: 'Libro',
+            required: [true, 'Ingresar codigo de libro']
+        },
+        codigoUsuario: {
+            type: Schema.Types.ObjectId,
+            ref: 'Usuario',
+            required: [true, 'Por favor ingresa el codigo de usuario']
+        },
+        fechaSalida: {
+            type: Date,
+            default: Date.now()
+        },
+        fechaDevolucion: {
+            type: Date
+        },
+        estado: {
+            type: Boolean,
+            default: true
+        }
+    });
 
-prestamoSchema.plugin(uniqueValidator, {
-    message: '{PATH} Debe ser único y diferente'
-});
+    prestamoSchema.plugin(uniqueValidator, {
+        message: '{PATH} Debe ser único y diferente'
+    });
 
-//crea una coleccion
-module.exports = mongoose.model('Prestamo', prestamoSchema);
+    //crea una coleccion
+    module.exports = mongoose.model('Prestamo', prestamoSchema);
